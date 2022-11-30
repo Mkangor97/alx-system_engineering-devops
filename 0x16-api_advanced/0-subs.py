e02323bc108bd284a2f8dac44acdc957f652c8f6
Mercy Kangor <41419454+Mkangor97@users.noreply.github.com>
2022-11-30 04:03:44 +0300

    Update 0-subs.py
    Updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,17 +6,17 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-        """
-                return number of subscribers for a given subreddit
-                        return 0 if invalid subreddit given
-                            """
-                                url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """
+        return number of subscribers for a given subreddit
+        return 0 if invalid subreddit given
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-                                    headers = requests.utils.default_headers()
-                                        headers.update({'User-Agent': 'My User Agent 1.0'})
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
 
-                                            r = requests.get(url, headers=headers).json()
-                                                subscribers = r.get('data', {}).get('subscribers')
-                                                    if not subscribers:
-                                                                return 0
-                                                                return subscribers
+    r = requests.get(url, headers=headers).json()
+    subscribers = r.get('data', {}).get('subscribers')
+    if not subscribers:
+        return 0
+    return subscribers
