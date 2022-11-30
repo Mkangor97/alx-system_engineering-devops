80cefed5bf343e685d5a484e7cd87201c78662c0
Mercy Kangor <41419454+Mkangor97@users.noreply.github.com>
2022-11-30 04:05:53 +0300

    Update 1-top_ten.py
    Updates

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,17 +6,17 @@ import requests
 
 
 def top_ten(subreddit):
-        """
-            return top ten titles for a given subreddit
-            return None if invalid subreddit given
-        """
-        headers = requests.utils.default_headers()
-        headers.update({'User-Agent': 'My User Agent 1.0'})
+    """
+        return top ten titles for a given subreddit
+        return None if invalid subreddit given
+    """
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
 
-        url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-        r = requests.get(url, headers=headers).json()
-        top_ten = r.get('data', {}).get('children', [])
-        if not top_ten:
-             print(None)
-        for t in top_ten:
-             print(t.get('data').get('title'))
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    r = requests.get(url, headers=headers).json()
+    top_ten = r.get('data', {}).get('children', [])
+    if not top_ten:
+        print(None)
+    for t in top_ten:
+        print(t.get('data').get('title'))
